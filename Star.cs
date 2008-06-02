e4dd12c31b3b4657a4aa8061bb0cdda2dad43efa
shanselman <none@none.com>
2008-06-02 18:49:35 +0000

    Initial commit

---
+++ Star.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Shapes;
+
+namespace BabySmash
+{
+    public class Star : Shape
+    {
+        // Using a DependencyProperty as the backing store for NumberOfPoints.     
+        public static readonly DependencyProperty NumberOfPointsProperty =
+            DependencyProperty.Register("NumberOfPoints", typeof(int), typeof(Shape), new UIPropertyMetadata(5));
+
+        public int NumberOfPoints
+        {
+            get { return (int)GetValue(NumberOfPointsProperty); }
+            set { SetValue(NumberOfPointsProperty, value); }
+        }
+        
+        protected override Geometry DefiningGeometry
+        {
+            get { return CreateStarGeometry(NumberOfPoints); }
+        }
+
+        public Geometry CreateStarGeometry(int numberOfPoints)
+        {
+            GeometryGroup group = new GeometryGroup();
+            group.FillRule = FillRule.Nonzero;
+            Geometry triangle = PathGeometry.Parse("M 0,-300 L 90,90 -90,90 0,-300");
+            group.Children.Add(triangle);
+
+            double deltaAngle = 360 / numberOfPoints;
+            double currentAngle = 0;
+            for (int index = 1; index < numberOfPoints; index++)
+            {
+                currentAngle += deltaAngle;
+                triangle = triangle.CloneCurrentValue();
+                triangle.Transform = new RotateTransform(currentAngle, 0, 0);
+                group.Children.Add(triangle);
+            }
+
+            Geometry outlinePath = group.GetOutlinedPathGeometry();
+            return outlinePath;
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BabySmash
+{
+    class Utils
+    {
+        private static Brush[] someBrushes = { Brushes.Red,
+                                  Brushes.Blue,
+                                  Brushes.Yellow,
+                                  Brushes.Green,
+                                  Brushes.Purple,
+                                  Brushes.Pink,
+                                  Brushes.Orange,
+                                  Brushes.Tan,
+                                  Brushes.Gray};
+
+        private static string[] sounds = { "giggle.wav", 
+                                            "babylaugh.wav",
+                                            "babygigl2.wav",
+                                            "ccgiggle.wav",
+                                            "laughingmice.wav",
+                                            "scooby2.wav",
+                                         };
+
+        public static Brush GetRandomColoredBrush()
+        {
+            Brush brush = someBrushes[lRandom.Next(0, someBrushes.Length)];
+            return  brush;
+        }
+
+        public static string GetRandomSoundFile()
+        {
+            string retVal = sounds[lRandom.Next(0, sounds.Length)];
+            return ".Resources.Sounds." + retVal;
+        }
+        public static bool GetRandomBoolean()
+        {
+            if (lRandom.Next(0, 2) == 0)
+                return false;
+            else
+                return true;
+        }
+
+        private static Random lRandom = new Random();
+
+        public static int RandomBetweenTwoNumbers(int min, int max)
+        {
+            return lRandom.Next(min, max + 1);
+        }
+
+        public static UIElement DrawCharacter(double fontSize, string textString, Brush brush, double left, double top)
+        {
+            HiResTextBlock textBlock = new HiResTextBlock()
+            {
+                //TextWrapping = TextWrapping.Wrap,
+                //FontFamily = new FontFamily("Rockwell Extra Bold"),
+                //Foreground = brush, //randomize
+                FontSize = fontSize, //pick better size
+                Fill = brush,
+                Text = textString,
+                StrokeThickness = 5,
+            };
+            textBlock.SetValue(Canvas.LeftProperty, left);
+            textBlock.SetValue(Canvas.TopProperty, top);
+            return textBlock;
+        }
+
+    }
+}
