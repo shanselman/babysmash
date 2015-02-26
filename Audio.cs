9adf621865af1784f75f178daa7ce8a2fd0b774e
Eric Lawrence <ericlaw1979@hotmail.com>
2015-02-25 19:47:32 -0600

    Play audio from memory
    There's no need for temp files.

--- Audio.cs
+++ Audio.cs
@@ -10,15 +10,9 @@ namespace BabySmash
     public class Win32Audio
     {
         /// <summary>
-        /// Collection of WAV resources we've stored on disk. The finalizer of the TempFileCollection will
-        /// attempt to delete these files for us.
+        /// Collection of soundname->WAV bytes mappings
         /// </summary>
-        TempFileCollection tempFiles = new TempFileCollection();
-
-        /// <summary>
-        /// Collection of soundname->filename mappings
-        /// </summary>
-        public static Dictionary<string, string> cachedWavs = new Dictionary<string, string>();
+        public static Dictionary<string, byte[]> cachedWavs = new Dictionary<string, byte[]>();
 
         /// <summary>
         /// Lock this object to protect against concurrent writes to the cachedWavs collection.
@@ -27,29 +21,28 @@ namespace BabySmash
 
         #region NativeAPI
         private const UInt32 SND_ASYNC = 0x0001;
+        private const UInt32 SND_MEMORY = 0x004;
         private const UInt32 SND_LOOP = 0x0008;
         private const UInt32 SND_NOSTOP = 0x0010;
 
         [DllImport("winmm.dll")]
         private static extern bool PlaySound(byte[] data, IntPtr hMod, UInt32 dwFlags);
 
-        [DllImport("winmm.dll", SetLastError = true)]
-        private static extern bool PlaySound(string pszSound, IntPtr hmod, UInt32 fdwSound);
         #endregion NativeAPI
 
         public void PlayWavResource(string wav)
         {
-            string s = GetWavResource(wav);
-            PlaySound(s, IntPtr.Zero, SND_ASYNC);
+            byte[] arrWav = GetWavResource(wav);
+            PlaySound(arrWav, IntPtr.Zero, SND_ASYNC | SND_MEMORY);
         }
 
         public void PlayWavResourceYield(string wav)
         {
-            string s = GetWavResource(wav);
-            PlaySound(s, IntPtr.Zero, SND_ASYNC | SND_NOSTOP);
+            byte[] arrWav = GetWavResource(wav);
+            PlaySound(arrWav, IntPtr.Zero, SND_ASYNC | SND_NOSTOP | SND_MEMORY);
         }
 
-        private string GetWavResource(string wav)
+        private byte[] GetWavResource(string wav)
         {
             if (cachedWavs.ContainsKey(wav))
             {
@@ -69,13 +62,10 @@ namespace BabySmash
                 // get the resource into a stream
                 using (Stream strm = Assembly.GetExecutingAssembly().GetManifestResourceStream(strName))
                 {
-                    string tempfile = System.IO.Path.GetTempFileName();
-                    tempFiles.AddFile(tempfile, false);
                     var arrWav = new Byte[strm.Length];
                     strm.Read(arrWav, 0, (int)strm.Length);
-                    File.WriteAllBytes(tempfile, arrWav);
-                    cachedWavs.Add(wav, tempfile);
-                    return tempfile;
+                    cachedWavs.Add(wav, arrWav);
+                    return arrWav;
                 }
             }
         }
