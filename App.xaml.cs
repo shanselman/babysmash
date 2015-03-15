2a1f1d09cdcafeab6387cc680a0a41ae7e441d41
karak <davidrieman@users.noreply.github.com>
2015-03-15 12:46:57 -0700

    Refactored key blocking into an AllowKeyboardInput check. * This method could be independently tested now, and should be more straight-forward. * Expanded the blocked keys to block more unwanted inputs. * Comment on the Volume Keys omission from the original (which seems intentional).

--- App.xaml.cs
+++ App.xaml.cs
@@ -51,37 +51,62 @@ namespace BabySmash
         {
             if (nCode >= 0)
             {
-                bool Alt = (WinForms.Control.ModifierKeys & Keys.Alt) != 0;
-                bool Control = (WinForms.Control.ModifierKeys & Keys.Control) != 0;
+                bool alt = (WinForms.Control.ModifierKeys & Keys.Alt) != 0;
+                bool control = (WinForms.Control.ModifierKeys & Keys.Control) != 0;
 
-                //Prevent ALT-TAB and CTRL-ESC by eating TAB and ESC. Also kill Windows Keys.
                 int vkCode = Marshal.ReadInt32(lParam);
                 Keys key = (Keys)vkCode;
 
-                if (Alt && key == Keys.F4)
+                if (alt && key == Keys.F4)
                 {
                     Application.Current.Shutdown();
-                    return (IntPtr)1; //handled
+                    return (IntPtr)1; // Handled.
                 }
-                if (key == Keys.LWin || key == Keys.RWin) return (IntPtr)1; //handled
-                if (Alt && key == Keys.Tab) return (IntPtr)1; //handled
-                if (Alt && key == Keys.Space) return (IntPtr)1; //handled
-                if (Control && key == Keys.Escape) return (IntPtr)1;
-                if (key == Keys.None) return (IntPtr)1; //handled
-                if (key <= Keys.Back) return (IntPtr)1; //handled
-                if (key == Keys.Menu) return (IntPtr)1; //handled
-                if (key == Keys.Pause) return (IntPtr)1; //handled
-                if (key == Keys.Help) return (IntPtr)1; //handled
-                if (key == Keys.Sleep) return (IntPtr)1; //handled
-                if (key == Keys.Apps) return (IntPtr)1; //handled
-                if (key >= Keys.KanaMode && key <= Keys.HanjaMode) return (IntPtr)1; //handled
-                if (key >= Keys.IMEConvert && key <= Keys.IMEModeChange) return (IntPtr)1; //handled
-                if (key >= Keys.BrowserBack && key <= Keys.BrowserHome) return (IntPtr)1; //handled
-                if (key >= Keys.MediaNextTrack && key <= Keys.OemClear) return (IntPtr)1; //handled
 
-                Debug.WriteLine(vkCode.ToString() + " " + key);
+                if (!AllowKeyboardInput(alt, control, key))
+                {
+                    return (IntPtr)1; // Handled.
+                }
             }
+
             return InterceptKeys.CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        /// <summary>Determines whether the specified keyboard input should be allowed to be processed by the system.</summary>
+        /// <remarks>Helps block unwanted keys and key combinations that could exit the app, make system changes, etc.</remarks>
+        public static bool AllowKeyboardInput(bool alt, bool control, Keys key)
+        {
+            // Disallow various special keys.
+            if (key <= Keys.Back || key == Keys.None ||
+                key == Keys.Menu || key == Keys.Pause ||
+                key == Keys.Help)
+            {
+                return false;
+            }
+
+            // Disallow ranges of special keys.
+            // Currently leaves volume controls enabled; consider if this makes sense.
+            // Disables non-existing Keys up to 65534, to err on the side of caution for future keyboard expansion.
+            if ((key >= Keys.LWin && key <= Keys.Sleep) ||
+                (key >= Keys.KanaMode && key <= Keys.HanjaMode) ||
+                (key >= Keys.IMEConvert && key <= Keys.IMEModeChange) ||
+                (key >= Keys.BrowserBack && key <= Keys.BrowserHome) ||
+                (key >= Keys.MediaNextTrack && key <= Keys.LaunchApplication2) ||
+                (key >= Keys.ProcessKey && key <= (Keys)65534))
+            {
+                return false;
+            }
+
+            // Disallow specific key combinations. (These component keys would be OK on their own.)
+            if ((alt && key == Keys.Tab) ||
+                (alt && key == Keys.Space) ||
+                (control && key == Keys.Escape))
+            {
+                return false;
+            }
+
+            // Allow anything else (like letters, numbers, spacebar, braces, and so on).
+            return true;
         }
     }
 }
