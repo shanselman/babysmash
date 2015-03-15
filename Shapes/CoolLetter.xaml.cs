add119349f363734769028a36c9072cbafe7f738
karak <davidrieman@users.noreply.github.com>
2015-03-15 00:48:55 -0700

    Cleaned up CoolLetter. Mainly autoformatting to fix inconsistencies, and sort-remove usings. Also fixed a typo.

--- Shapes/CoolLetter.xaml.cs
+++ Shapes/CoolLetter.xaml.cs
@@ -1,40 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using BabySmash.Properties;
 
 namespace BabySmash
 {
-	/// <summary>   
-	/// Interaction logic for CoolCircle.xaml
-	/// </summary>
-   [Serializable]
-   public partial class CoolLetter 
-	{
-      public CoolLetter()
-      {
-         this.InitializeComponent();
-      }
-		public CoolLetter(Brush x, string letter) : this()
-		{
-			
+    /// <summary>   
+    /// Interaction logic for CoolLetter.xaml
+    /// </summary>
+    [Serializable]
+    public partial class CoolLetter
+    {
+        public CoolLetter()
+        {
+            this.InitializeComponent();
+        }
 
-		    this.letterPath.Fill = x;
+        public CoolLetter(Brush x, string letter)
+            : this()
+        {
+            this.letterPath.Fill = x;
 
-		    this.letterPath.Data = MakeCharacterGeometry(GetLetterCharacter(letter));
-          this.Width = this.letterPath.Data.Bounds.Width + this.letterPath.Data.Bounds.X + this.letterPath.StrokeThickness / 2;
-          this.Height = this.letterPath.Data.Bounds.Height + this.letterPath.Data.Bounds.Y + this.letterPath.StrokeThickness / 2;
-		}
+            this.letterPath.Data = MakeCharacterGeometry(GetLetterCharacter(letter));
+            this.Width = this.letterPath.Data.Bounds.Width + this.letterPath.Data.Bounds.X + this.letterPath.StrokeThickness / 2;
+            this.Height = this.letterPath.Data.Bounds.Height + this.letterPath.Data.Bounds.Y + this.letterPath.StrokeThickness / 2;
+        }
 
         private static Geometry MakeCharacterGeometry(string t)
         {
@@ -66,6 +57,5 @@ namespace BabySmash
             }
             return nameToDisplay;
         }
-
-	}
+    }
 }
