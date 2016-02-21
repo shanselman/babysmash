9925f292384df4aca4ff9a5d559af51f2e32a378
David Rieman <davidrieman@msn.com>
2016-02-20 18:35:03 -0800

    Restored auto-formatting consistency, and removed some unused usings.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,6 @@
-﻿
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-
 using System.Windows.Media;
 
 namespace BabySmash
@@ -14,7 +12,7 @@ namespace BabySmash
 
         private UserControl customCursor;
         public UserControl CustomCursor { get { return customCursor; } set { customCursor = value; } }
-        
+
         public void AddFigure(UserControl c)
         {
             this.figuresCanvas.Children.Add(c);
@@ -22,7 +20,7 @@ namespace BabySmash
 
         public void RemoveFigure(UserControl c)
         {
-           this.figuresCanvas.Children.Remove(c);
+            this.figuresCanvas.Children.Remove(c);
         }
 
         public MainWindow(Controller c)
@@ -36,52 +34,52 @@ namespace BabySmash
 
         protected override void OnMouseWheel(MouseWheelEventArgs e)
         {
-           base.OnMouseWheel(e);
-           controller.MouseWheel(this, e);
+            base.OnMouseWheel(e);
+            controller.MouseWheel(this, e);
         }
 
         protected override void OnMouseUp(MouseButtonEventArgs e)
         {
-           base.OnMouseUp(e);
-           controller.MouseUp(this, e);
+            base.OnMouseUp(e);
+            controller.MouseUp(this, e);
         }
 
         protected override void OnMouseDown(MouseButtonEventArgs e)
         {
-           base.OnMouseDown(e);
-           controller.MouseDown(this, e);
+            base.OnMouseDown(e);
+            controller.MouseDown(this, e);
         }
 
         protected override void OnMouseEnter(MouseEventArgs e)
         {
-           base.OnMouseEnter(e);
-           AssertCursor();
-           CustomCursor.Visibility = Visibility.Visible;
+            base.OnMouseEnter(e);
+            AssertCursor();
+            CustomCursor.Visibility = Visibility.Visible;
         }
 
         protected override void OnMouseLeave(MouseEventArgs e)
         {
-           base.OnMouseLeave(e);
-           CustomCursor.Visibility = Visibility.Hidden;
+            base.OnMouseLeave(e);
+            CustomCursor.Visibility = Visibility.Hidden;
         }
 
         protected override void OnMouseMove(MouseEventArgs e)
         {
-           base.OnMouseMove(e);
-           if (controller.isOptionsDialogShown == false)
-           {
-               CustomCursor.Visibility = Visibility.Visible;
-               Point p = e.GetPosition(mouseDragCanvas);
-               double pX = p.X;
-               double pY = p.Y;
-               Cursor = Cursors.None;
-               Canvas.SetTop(CustomCursor, pY);
-               Canvas.SetLeft(CustomCursor, pX);
-               Canvas.SetZIndex(CustomCursor, int.MaxValue);
-           }
-           controller.MouseMove(this, e);
+            base.OnMouseMove(e);
+            if (controller.isOptionsDialogShown == false)
+            {
+                CustomCursor.Visibility = Visibility.Visible;
+                Point p = e.GetPosition(mouseDragCanvas);
+                double pX = p.X;
+                double pY = p.Y;
+                Cursor = Cursors.None;
+                Canvas.SetTop(CustomCursor, pY);
+                Canvas.SetLeft(CustomCursor, pX);
+                Canvas.SetZIndex(CustomCursor, int.MaxValue);
+            }
+            controller.MouseMove(this, e);
         }
-        
+
         protected override void OnKeyUp(KeyEventArgs e)
         {
             base.OnKeyUp(e);
@@ -97,24 +95,23 @@ namespace BabySmash
 
         internal void AssertCursor()
         {
-           try
-           {
-              mouseCursorCanvas.Children.Clear();
-              CustomCursor = Utils.GetCursor();
-              if (CustomCursor.Parent != null)
-              {
-                 ((Canvas)CustomCursor.Parent).Children.Remove(CustomCursor);
-              }
-              CustomCursor.RenderTransform = new ScaleTransform(0.5, 0.5);
-              CustomCursor.Name = "customCursor";
-              mouseCursorCanvas.Children.Insert(0, CustomCursor); //in front!
-              CustomCursor.Visibility = Visibility.Hidden;
-           }
-           catch (System.NotSupportedException)
-           {
-              //we can die here if we ALT-F4 while in the Options Dialog
-           }
-
+            try
+            {
+                mouseCursorCanvas.Children.Clear();
+                CustomCursor = Utils.GetCursor();
+                if (CustomCursor.Parent != null)
+                {
+                    ((Canvas)CustomCursor.Parent).Children.Remove(CustomCursor);
+                }
+                CustomCursor.RenderTransform = new ScaleTransform(0.5, 0.5);
+                CustomCursor.Name = "customCursor";
+                mouseCursorCanvas.Children.Insert(0, CustomCursor); //in front!
+                CustomCursor.Visibility = Visibility.Hidden;
+            }
+            catch (System.NotSupportedException)
+            {
+                //we can die here if we ALT-F4 while in the Options Dialog
+            }
         }
     }
 }
--- Shapes/FigureGenerator.cs
+++ Shapes/FigureGenerator.cs
@@ -10,93 +10,93 @@ using BabySmash.Shapes;
 
 namespace BabySmash
 {
-	public class FigureTemplate
-	{
-		public Brush Fill { get; set; }
-		public Color Color { get; set; }
-		public BrushControlFunc GeneratorFunc { get; set; }
-		public Effect Effect { get; set; }
-		public string Name { get; set; }
-		public string Letter { get; set; }
-	}
+    public class FigureTemplate
+    {
+        public Brush Fill { get; set; }
+        public Color Color { get; set; }
+        public BrushControlFunc GeneratorFunc { get; set; }
+        public Effect Effect { get; set; }
+        public string Name { get; set; }
+        public string Letter { get; set; }
+    }
 
-	public class FigureGenerator
-	{
-		private static readonly List<KeyValuePair<BabySmashShape, BrushControlFunc>> hashTableOfFigureGenerators = new List<KeyValuePair<BabySmashShape, BrushControlFunc>>
-			 {
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Circle, x => new CoolCircle(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Oval, x => new CoolOval(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Rectangle, x => new CoolRectangle(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Hexagon, x => new CoolHexagon(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Trapezoid, x => new CoolTrapezoid(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Star, x => new CoolStar(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Square, x => new CoolSquare(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Triangle, x => new CoolTriangle(x) ),
-					 new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Heart, x => new CoolHeart(x) )
-			 };
+    public class FigureGenerator
+    {
+        private static readonly List<KeyValuePair<BabySmashShape, BrushControlFunc>> hashTableOfFigureGenerators = new List<KeyValuePair<BabySmashShape, BrushControlFunc>>
+             {
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Circle, x => new CoolCircle(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Oval, x => new CoolOval(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Rectangle, x => new CoolRectangle(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Hexagon, x => new CoolHexagon(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Trapezoid, x => new CoolTrapezoid(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Star, x => new CoolStar(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Square, x => new CoolSquare(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Triangle, x => new CoolTriangle(x) ),
+                     new KeyValuePair<BabySmashShape, BrushControlFunc>(BabySmashShape.Heart, x => new CoolHeart(x) )
+             };
 
-		public static UserControl NewUserControlFrom(FigureTemplate template)
-		{
-			UserControl retVal = null;
+        public static UserControl NewUserControlFrom(FigureTemplate template)
+        {
+            UserControl retVal = null;
 
-			if (template.Letter.Length == 1 && Char.IsLetterOrDigit(template.Letter[0]))
-			{
-				retVal = new CoolLetter(template.Fill.Clone(), template.Letter[0]);
-			}
-			else
-			{
-				retVal = template.GeneratorFunc(template.Fill.Clone());
-			}
+            if (template.Letter.Length == 1 && Char.IsLetterOrDigit(template.Letter[0]))
+            {
+                retVal = new CoolLetter(template.Fill.Clone(), template.Letter[0]);
+            }
+            else
+            {
+                retVal = template.GeneratorFunc(template.Fill.Clone());
+            }
 
-			var randomTransition1 = (Tweener.TransitionType)Utils.RandomBetweenTwoNumbers(1, (int)Tweener.TransitionType.EaseOutInBounce);
-			var ani1 = Tweener.Tween.CreateAnimation(randomTransition1, 0, 1, new TimeSpan(0, 0, 0, 1), 30);
-			var randomTransition2 = (Tweener.TransitionType)Utils.RandomBetweenTwoNumbers(1, (int)Tweener.TransitionType.EaseOutInBounce);
-			var ani2 = Tweener.Tween.CreateAnimation(randomTransition2, 360, 0, new TimeSpan(0, 0, 0, 1), 30);
-			retVal.RenderTransformOrigin = new Point(0.5, 0.5);
-			var group = new TransformGroup();
-			group.Children.Add(new ScaleTransform());
-			group.Children.Add(new RotateTransform());
-			retVal.RenderTransform = group;
-			group.Children[0].BeginAnimation(ScaleTransform.ScaleXProperty, ani1);
-			group.Children[0].BeginAnimation(ScaleTransform.ScaleYProperty, ani1);
-			group.Children[1].BeginAnimation(RotateTransform.AngleProperty, ani2);
+            var randomTransition1 = (Tweener.TransitionType)Utils.RandomBetweenTwoNumbers(1, (int)Tweener.TransitionType.EaseOutInBounce);
+            var ani1 = Tweener.Tween.CreateAnimation(randomTransition1, 0, 1, new TimeSpan(0, 0, 0, 1), 30);
+            var randomTransition2 = (Tweener.TransitionType)Utils.RandomBetweenTwoNumbers(1, (int)Tweener.TransitionType.EaseOutInBounce);
+            var ani2 = Tweener.Tween.CreateAnimation(randomTransition2, 360, 0, new TimeSpan(0, 0, 0, 1), 30);
+            retVal.RenderTransformOrigin = new Point(0.5, 0.5);
+            var group = new TransformGroup();
+            group.Children.Add(new ScaleTransform());
+            group.Children.Add(new RotateTransform());
+            retVal.RenderTransform = group;
+            group.Children[0].BeginAnimation(ScaleTransform.ScaleXProperty, ani1);
+            group.Children[0].BeginAnimation(ScaleTransform.ScaleYProperty, ani1);
+            group.Children[1].BeginAnimation(RotateTransform.AngleProperty, ani2);
 
-			if (Settings.Default.BitmapEffects)
-			{
-				retVal.Effect = template.Effect.Clone();
-			}
+            if (Settings.Default.BitmapEffects)
+            {
+                retVal.Effect = template.Effect.Clone();
+            }
 
-			return retVal;
-		}
+            return retVal;
+        }
 
-		//TODO: Should this be in XAML? Would that make it better?
-		//TODO: Should I change the height, width and stroke to be relative to the screen size?
-		//TODO: Where can I get REALLY complex shapes like animal vectors or custom pics? Where do I store them?
+        //TODO: Should this be in XAML? Would that make it better?
+        //TODO: Should I change the height, width and stroke to be relative to the screen size?
+        //TODO: Where can I get REALLY complex shapes like animal vectors or custom pics? Where do I store them?
 
-		public static FigureTemplate GenerateFigureTemplate(char displayChar)
-		{
-			Color c = Utils.GetRandomColor();
+        public static FigureTemplate GenerateFigureTemplate(char displayChar)
+        {
+            Color c = Utils.GetRandomColor();
 
-			string name = null;
-			KeyValuePair<BabySmashShape, BrushControlFunc> nameFunc = hashTableOfFigureGenerators[Utils.RandomBetweenTwoNumbers(0, hashTableOfFigureGenerators.Count - 1)];
-			if (Char.IsLetterOrDigit(displayChar))
-			{
-				name = displayChar.ToString();
-			}
-			else
-			{
-				name = Controller.GetLocalizedString(nameFunc.Key.ToString());
-			}
+            string name = null;
+            KeyValuePair<BabySmashShape, BrushControlFunc> nameFunc = hashTableOfFigureGenerators[Utils.RandomBetweenTwoNumbers(0, hashTableOfFigureGenerators.Count - 1)];
+            if (Char.IsLetterOrDigit(displayChar))
+            {
+                name = displayChar.ToString();
+            }
+            else
+            {
+                name = Controller.GetLocalizedString(nameFunc.Key.ToString());
+            }
 
-			return new FigureTemplate
-			{
-				Color = c,
-				Name = name,
-				GeneratorFunc = nameFunc.Value,
-				Fill = Utils.GetGradientBrush(c),
-				Letter = displayChar.ToString(),
-				Effect = Animation.GetRandomBitmapEffect()
-			};
-		}
-	}
+            return new FigureTemplate
+            {
+                Color = c,
+                Name = name,
+                GeneratorFunc = nameFunc.Value,
+                Fill = Utils.GetGradientBrush(c),
+                Letter = displayChar.ToString(),
+                Effect = Animation.GetRandomBitmapEffect()
+            };
+        }
+    }
 }
